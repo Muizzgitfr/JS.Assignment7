0f492012f91a1070ea1aaa90cc82c0aeec95b253
Abdul Muizz <163648314+Muizzgitfr@users.noreply.github.com>
2024-10-13 15:00:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+//  Assignment 1
+// var cityToCheck = "karachi";
+// // var cityToCheck1 = "Lahore";
+// var firstChar = cityToCheck.slice(0, 1);
+// var otherChars = cityToCheck.slice(1);
+// firstChar = firstChar.toUpperCase();
+// otherChars = otherChars.toLowerCase();
+// var cappedCity = firstChar + otherChars;
+// console.log(cappedCity);
+
+
+// Assignment 2
+// var month = prompt("Enter a month");
+// var charsInMonth = month.length;
+// if (charsInMonth > 5) {
+// monthAbbrev = month.slice(0, 3);
+// console.log(monthAbbrev);
+//  }
+
+
+// // Assignment 3
+// let str = "JavaScript Programming";
+// // Measuring string length
+// let length = str.length;
+// console.log(length);  // Output: 22
+
+
+// Assignment 4
+// Extracting parts of the string using slice
+// let str = "Java Programming";
+// let part = str.slice(0, 10);  // Extracts from index 0 to 9 (10 characters)
+// console.log(part);  // Output: "JavaScript"
+
+
+// Assignment 5
+//  Extracting using substring
+// let str = "JavaScript Programming";
+// let part2 = str.substring(4, 14);  // Extracts from index 4 to 13
+// console.log(part2);  // Output: "Script Pro"
